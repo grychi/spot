e429e3850636c7b339ca94aeea1b82dd32a38a86
Gary Chi <gc1753@nyu.edu>
2018-09-08 15:50:13 -0400

    login and register styling B)

--- www/js/register.js
+++ www/js/register.js
@@ -1,20 +1,19 @@
 
 var baseUrl = "http://10.251.80.142:8012"
 
-function toLogin(){
+function toLogin() {
     var tmp = {
-                username: document.getElementById("username").value,
-                password: document.getElementById("password").value
-            }
-        postJSON(tmp, baseUrl + "/register", function (res) {
-            if (res.success) {
-                location.href = "login.html";
-            }
-            else {
-                // show error
-                console.log("wait something wrong xD");
-            }
-        });
+        username: document.getElementById("username").value,
+        password: document.getElementById("password").value
+    }
+    postJSON(tmp, baseUrl + "/register", function (res) {
+        if (res.success) {
+            showToast('You have successfully registered. <a href="login.html">Click here to login.</a>');
+        }
+        else {
+            showToast(res.error);
+        }
+    });
 }
 
 document.addEventListener("DOMContentLoaded", function (e) {
@@ -22,16 +21,18 @@ document.addEventListener("DOMContentLoaded", function (e) {
 
         var pass1 = document.getElementById("password").value;
         var pass2 = document.getElementById("pass_check").value;
-        var ok = true;
+        if (pass1.value == '' || document.getElementById("username").value == '') {
+            showToast('Please enter required fields.');
+            return;
+        }
         if (pass1 != pass2) {
             document.getElementById("password").style.borderColor = "#E34234";
             document.getElementById("pass_check").style.borderColor = "#E34234";
-            return; 
+            showToast("Passwords do not match. Try again.")
+            return;
         }
         else {
-            console.log("Passwords Match!!!");
             toLogin();
-
         }
     });
     document.addEventListener('keypress', function (e) {
@@ -70,3 +71,11 @@ function postJSON(data, url, loaded) {
     xhr.send(JSON.stringify(data));
 }
 
+function showToast(text = "") {
+    document.getElementById("tMessage").innerHTML = text;
+    document.getElementById("toast").style.display = "block";
+}
+
+function hideToast() {
+    document.getElementById("toast").style.display = "none";
+}
